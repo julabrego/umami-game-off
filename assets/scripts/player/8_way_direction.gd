36a825ef7dc6b230876bc686778639dc42007635
julabrego <abrego.jul@gmail.com>
2023-11-02 16:41:47 -0300

    first commit

---
+++ assets/scripts/player/8_way_direction.gd
@@ -0,0 +1,22 @@
+extends CharacterBody2D
+
+@export var speed = 400
+
+var slope_detector = Vector2(10,100)
+
+func get_input():
+	velocity = Vector2()
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("ui_up"):
+		velocity.y -= 1
+	velocity = velocity.normalized() * speed
+
+func _physics_process(delta):
+	get_input()
+	move_and_slide()
+
